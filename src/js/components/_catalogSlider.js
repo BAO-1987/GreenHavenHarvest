221280f1b007f1b234d97c0f6eee09c719766bcd
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-04-11 23:19:17 +0300

    home pixel

---
+++ src/js/components/_catalogSlider.js
@@ -0,0 +1,20 @@
+import Swiper, {
+  Navigation,
+  Pagination,
+} from 'swiper';
+
+const slider = new Swiper('.catalog__swiper', {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  modules: [Navigation, Pagination],
+  draggable: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.catalog__swiper .btn--next',
+    prevEl: '.catalog__swiper .btn--prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
